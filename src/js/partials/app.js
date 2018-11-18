985e3dd5f0f4f79aef91407e6bfee17baf912458
prosp3ro <prospero1135@gmail.com>
2018-11-18 02:25:50 +0200

    +slider (no responsiv, no failure)

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,12 @@
 //Some JS
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        prevArrow: $('.arrow.arrow--left'),
+        nextArrow: $('.arrow.arrow--right'),
+    });
+});
