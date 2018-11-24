631a2fdcb0717b0425938d1398609c259cb8648e
prosp3ro <prospero1135@gmail.com>
2018-11-24 20:50:43 +0200

    +carousel fix

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,12 +1,38 @@
 //Some JS
 $(document).ready(function(){
-    $('.carousel').slick({
+    var carousel = $('.carousel');
+    var WIN = $(window);
+    var breakpoint = 1200;
+    var screenWidth =  WIN.outerWidth();
+    var isSlick = false;
+    var slickConfig = {
         infinite: true,
         slidesToShow: 7,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: $('.arrow.arrow--left'),
-        nextArrow: $('.arrow.arrow--right'),
-    });
+        prevArrow: '<div class="arrow arrow--left"></div>',
+        nextArrow: '<div class="arrow arrow--right"></div>',
+    }
+    if(screenWidth>1200){
+        
+        carousel.slick(slickConfig);
+        isSlick = true;
+    }
+
+    WIN.resize(function() {
+        var screenWidth = WIN.outerWidth();
+        if(screenWidth>breakpoint){
+        isSlick && carousel.slick('destroy');
+         isSlick = false;
+
+        }else{
+
+           !isSlick && carousel.slick(slickConfig);
+            isSlick = true;
+
+        }
+        
+    })
+    
 });
