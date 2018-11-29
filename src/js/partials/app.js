ff1e0fc5a0dc9d003a15fea5deb0f522b81d85a2
prosp3ro <prospero1135@gmail.com>
2018-11-29 23:45:29 +0200

    +slick fix

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,38 +1,32 @@
 //Some JS
-$(document).ready(function(){
-    var carousel = $('.carousel');
+$(document).ready(function() {
+    var carousel = $(".carousel");
     var WIN = $(window);
     var breakpoint = 1200;
-    var screenWidth =  WIN.outerWidth();
+    var screenWidth = WIN.outerWidth();
     var isSlick = false;
     var slickConfig = {
-        infinite: true,
-        slidesToShow: 7,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<div class="arrow arrow--left"></div>',
-        nextArrow: '<div class="arrow arrow--right"></div>',
+      infinite: true,
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      prevArrow: '<div class="arrow arrow--left"></div>',
+      nextArrow: '<div class="arrow arrow--right"></div>'
+    };
+    if (screenWidth > 1200) {
+      carousel.slick(slickConfig);
+      isSlick = true;
     }
-    if(screenWidth>1200){
-        
-        carousel.slick(slickConfig);
-        isSlick = true;
-    }
-
+  
     WIN.resize(function() {
-        var screenWidth = WIN.outerWidth();
-        if(screenWidth>breakpoint){
-        isSlick && carousel.slick('destroy');
-         isSlick = false;
-
-        }else{
-
-           !isSlick && carousel.slick(slickConfig);
-            isSlick = true;
-
-        }
-        
-    })
-    
-});
+      var screenWidth = WIN.outerWidth();
+      if (screenWidth < breakpoint) {
+        isSlick && carousel.slick("destroy");
+        isSlick = false;
+      } else {
+        !isSlick && carousel.slick(slickConfig);
+        isSlick = true;
+      }
+    });
+  });
